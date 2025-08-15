6c5bd283c35bfa9fc34ace25948c8517883ccca7
Juan Garces <62315124+juangarces1@users.noreply.github.com>
2025-08-15 15:59:05 -0500

    Agosto

--- lib/Helpers/constans.dart
+++ lib/Helpers/constans.dart
@@ -1,5 +1,5 @@
 class Constans {
   // Api Barbosa no Borrar
- // static String get apiUrl => 'http://192.168.1.50:80'; 
-  static String get apiUrl => 'http://192.168.1.165:8085'; 
+  static String get apiUrl => 'http://192.168.0.150:80'; 
+ // static String get apiUrl => 'http://192.168.1.165:8085'; 
 }
